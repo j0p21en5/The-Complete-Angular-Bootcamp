cdfe30522b015b91676a9e03e946a2e19ad14676
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-03-13 22:35:06 +0530

    ad code
    component with inline style and template

---
+++ ComponentwithInlineStyle_Template/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { CountryListComponent } from './country-list/country-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    StudentListComponent,
+    CountryListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ComponentwithInlineStyle_Template/src/app/country-list/country-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-list',
+  template: `
+    <h2 class="custom">
+      country-list works!
+    </h2>
+  `,
+  styles: [
+    `.custom{
+      color:blue;
+    }`
+  ]
+})
+export class CountryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
