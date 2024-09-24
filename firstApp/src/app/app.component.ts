a5c6d22909323894f75520b1be58690de56ec737
Jaypatel93 <jaykumarpatel881@gmail.com>
2024-09-24 22:44:30 +0530

    update
    the new file is added

---
+++ firstApp/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firstApp';
+
+
+  userData:any={}
+
+  getData(data:NgForm){
+    console.warn(data)
+    this.userData=data
+
+  }
+
+  show=false;
+
+  toggle(){
+    this.show=!this.show;
+  }
+
+
+
+  // Nested Loop
+  // users = [
+  //   {name:'jay',phone : '8817',socialAccounts:['insta','fb','gmail']},
+  //   {name : 'yash',phone:'9399',socialAccounts:['linkedin','yahoo','gmail']}
+  // ]
+
+  // num:any = [4,5,9,8,5,2,4
+
+  // ]
+
+  // style binding
+
+  // color="red";
+  // bgColor="yellow";
+
+  // updateColor(){
+  //   this.color="blue";
+  //   this.bgColor="green";
+  // }
+
+  // divn = "blue";
+  // bgdivn ="red"
+  // changeColor(){
+  //   this.divn="red";
+  //   this.bgdivn="blue"
+  // }
+
+
+  
+}
